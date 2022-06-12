65d817c5343ea1d9c576e24e674097283bc1d6b2
Vardaan Aggarwal <98269336+VardaanAggarwal@users.noreply.github.com>
2022-06-12 17:16:39 +0530

    Update CanForPaint.py

--- Day-8/CanForPaint.py
+++ Day-8/CanForPaint.py
@@ -1,8 +1,9 @@
+import math
 print("Welcome to the wall paint services.")
 print("- We can paint 5 square meters of your wall with 1 can of paint.")
 print("- Price of 1 single paint can is $13.")
 height_of_wall = int(input("Enter the height of the wall : "))
 width_of_wall = int(input("Enter the width of the wall : "))
-total_cans = round((height_of_wall*width_of_wall)/5)
+total_cans = math.ceil((height_of_wall*width_of_wall)/5)
 total_price = total_cans*13
 print(f"Total price for painting your walls would cost you about ${total_price}!!")
